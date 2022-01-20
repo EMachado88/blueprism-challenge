fc4751484461e38c18cf159329312a8737b05c2e
Emanuel Machado <emanuel@emachado.pt>
2022-01-20 16:52:36 +0000

    Added extra info to schedule

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -3,7 +3,9 @@ const Schedule = ({
     name,
     description,
     id,
-    isRetired
+    isRetired,
+    tasksCount,
+    intervalType
   },
   handleClickSchedule,
   selectedSchedule,
@@ -16,7 +18,9 @@ const Schedule = ({
       >
         <h3 className='schedule-name'>{name}</h3>
         <p className='schedule-description'>{description}</p>
+        <p className='schedule-interval'><strong>Interval:</strong> {intervalType}</p>
         <div className='schedule-button'>
+          <span><strong>Tasks:</strong> {tasksCount}</span>
           <button onClick={toggleIsRetired(id)}>{isRetired ? 'Unretire' : 'Retire'}</button>
         </div>
       </div>
