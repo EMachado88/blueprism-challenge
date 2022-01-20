809a56e9e9204eb37194b636aeda81fb2a4711cc
Emanuel Machado <emanuel@emachado.pt>
2022-01-20 17:29:50 +0000

    Completed logs info and styling

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './helpers.css'
 import './App.css'
 
 import Header from './components/Header'
--- src/components/Log.js
+++ src/components/Log.js
@@ -1,13 +1,23 @@
 const Log = ({ 
   log: {
+    id,
     serverName,
-    status
+    status,
+    startTime,
+    endTime
   }
 }) => {
+  const startDate = (new Date(startTime)).toLocaleDateString()
+  const endDate = (new Date(endTime)).toLocaleDateString()
+
   return (
     <div className='log'>
-      <h3>{serverName}</h3>
-      <p>{status}</p>
+      <h3 className='text-center'>{serverName}</h3>
+      <hr/>
+      <p><strong>id:</strong> <span>{id}</span></p>
+      <p><strong>Start:</strong> <span>{startDate}</span></p>
+      <p><strong>End:</strong> <span>{endDate}</span></p>
+      <p><strong>Status:</strong> <span>{status}</span></p>
     </div>
   )
 }
