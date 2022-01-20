c0a3f3c6b6f017a663827ef60daba3b8daa1bab2
Emanuel Machado <emanuel@emachado.pt>
2022-01-20 23:37:44 +0000

    Added snapshot test for App

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import renderer from 'react-test-renderer'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from './App'
+
+test('Renders App correctly', () => {
+  const tree = renderer
+    .create(
+      <App />,
+    )
+    .toJSON()
+
+  expect(tree).toMatchSnapshot()
 });
