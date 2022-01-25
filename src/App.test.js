5d0523d53664b8f240daf8d2d762fc5c372466bc
Emanuel Machado <emanuel@emachado.pt>
2022-01-25 02:23:52 +0000

    Updated snapshot, described test

--- src/App.test.js
+++ src/App.test.js
@@ -2,12 +2,14 @@ import renderer from 'react-test-renderer'
 
 import App from './App'
 
-test('Renders App correctly', () => {
-  const tree = renderer
-    .create(
-      <App />,
-    )
-    .toJSON()
-
-  expect(tree).toMatchSnapshot()
-});
+describe('App', () => {
+  it('Renders correctly', () => {
+    const tree = renderer
+      .create(
+        <App />,
+      )
+      .toJSON()
+  
+    expect(tree).toMatchSnapshot()
+  })
+})
